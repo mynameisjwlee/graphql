53a6cc3b7f82c0ceec594e6bee8ea52349b8c6e5
wook <>
2020-12-28 21:48:06 +0900

    graphql example

--- index.ts
+++ index.ts
@@ -1,10 +1,38 @@
 import express from 'express';
 import TestApi from './apis/rest/test.api';
 import APIS from './apis';
+import { ApolloServer, gql } from 'apollo-server-express';
 
 const app = express();
+app.use(express.json());
 
-const restTest = new TestApi;
+const schema = gql`
+  type Query {
+    me: User
+  }
+ 
+  type User {
+    username: String!
+  }
+`;
+ 
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Robin Wieruch',
+      };
+    },
+  },
+};
+
+const gqlServer = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+});
+gqlServer.applyMiddleware({ app, path: '/graphql' });
+
+const restTest = new TestApi();
 app.get(APIS.REST_TEST, restTest.get);
 
 app.listen(3000);
--- toJS/index.js
+++ toJS/index.js
@@ -6,7 +6,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const test_api_1 = __importDefault(require("./apis/rest/test.api"));
 const apis_1 = __importDefault(require("./apis"));
+const apollo_server_express_1 = require("apollo-server-express");
 const app = express_1.default();
-const restTest = new test_api_1.default;
+app.use(express_1.default.json());
+const schema = apollo_server_express_1.gql `
+  type Query {
+    me: User
+  }
+ 
+  type User {
+    username: String!
+  }
+`;
+const resolvers = {
+    Query: {
+        me: () => {
+            return {
+                username: 'Robin Wieruch',
+            };
+        },
+    },
+};
+const gqlServer = new apollo_server_express_1.ApolloServer({
+    typeDefs: schema,
+    resolvers,
+});
+gqlServer.applyMiddleware({ app, path: '/graphql' });
+const restTest = new test_api_1.default();
 app.get(apis_1.default.REST_TEST, restTest.get);
 app.listen(3000);
