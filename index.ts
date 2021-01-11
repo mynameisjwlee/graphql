f9abecf30ed2f7ff52bb621d3083f8f97ba7ea06
poerty <s2peters2@naver.com>
2021-01-11 21:55:17 +0900

    Add some stuff

--- index.ts
+++ index.ts
@@ -6,21 +6,31 @@ import { ApolloServer } from 'apollo-server-express';
 import { BookResolver } from './apis/gql/resolvers/book.resolver';
 import { StudentResolver } from './apis/gql/resolvers/student.resolver';
 
-const app = express();
-app.use(express.json());
 
-const gqlSchema = buildSchemaSync({
-  resolvers: [
-    BookResolver,
-    StudentResolver,
-  ],
-  validate: false,
-});
+function startServer() {
+  const app = express();
+  app.use(express.json());
 
-const gqlServer = new ApolloServer({ schema: gqlSchema });
-gqlServer.applyMiddleware({ app, path: '/graphql' });
+  const gqlSchema = buildSchemaSync({
+    resolvers: [
+      BookResolver,
+      StudentResolver,
+    ],
+    validate: false,
+  });
 
-const restTest = new TestApi();
-app.get(APIS.TEST, restTest.get);
+  const gqlServer = new ApolloServer({ schema: gqlSchema });
+  gqlServer.applyMiddleware({ app, path: '/graphql' });
 
-app.listen(3000);
+  const restTest = new TestApi();
+  app.get(APIS.TEST, restTest.get);
+
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`[${new Date().toISOString()}] Started at http://localhost:${port}/graphql`);
+  });
+}
+
+(() => {
+  startServer()
+})()
